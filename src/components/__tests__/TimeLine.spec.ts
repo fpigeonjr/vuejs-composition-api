185ac9104ddf44879ce1004391268dbae8de0e1c
Frank Pigeon <frank.pigeonjr@gmail.com>
2022-03-07 21:39:09 -0600

    adds more tests

--- src/components/__tests__/TimeLine.spec.ts
+++ src/components/__tests__/TimeLine.spec.ts
@@ -1,13 +1,27 @@
 import { describe, it, expect } from "vitest"
 import { mount } from "@vue/test-utils"
 import TimeLine from "@/components/TimeLine.vue"
-import { today } from "../../mocks"
+import { today, thisWeek, thisMonth } from "../../mocks"
 
 describe("<TimeLine>", () => {
   it("renders today post default", () => {
     const wrapper = mount(TimeLine)
-
     console.log(wrapper.html())
     expect(wrapper.html()).toContain(today.created.format("Do MMM"))
+  })
+
+  it("updates when the week period is clicked", async () => {
+    const wrapper = mount(TimeLine)
+    await wrapper.get('[data-test="This Week"]').trigger("click")
+    expect(wrapper.html()).toContain(today.created.format("Do MMM"))
+    expect(wrapper.html()).toContain(thisWeek.created.format("Do MMM"))
+  })
+
+  it("updates when the month period is clicked", async () => {
+    const wrapper = mount(TimeLine)
+    await wrapper.get('[data-test="This Month"]').trigger("click")
+    expect(wrapper.html()).toContain(today.created.format("Do MMM"))
+    expect(wrapper.html()).toContain(thisWeek.created.format("Do MMM"))
+    expect(wrapper.html()).toContain(thisMonth.created.format("Do MMM"))
   })
 })
