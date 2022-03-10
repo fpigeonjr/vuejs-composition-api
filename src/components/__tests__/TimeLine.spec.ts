d082f31b2adff325b19405c246b4793be218da1e
Frank Pigeon <frank.pigeonjr@gmail.com>
2022-03-09 19:44:54 -0600

    wip

--- src/components/__tests__/TimeLine.spec.ts
+++ src/components/__tests__/TimeLine.spec.ts
@@ -1,11 +1,40 @@
-import { describe, it, expect } from "vitest"
-import { mount } from "@vue/test-utils"
+import axios from "axios"
+import { describe, it, expect, vi } from "vitest"
+import { mount, flushPromises } from "@vue/test-utils"
 import TimeLine from "@/components/TimeLine.vue"
 import { today, thisWeek, thisMonth } from "../../mocks"
 
+vi.mock("axios")
+
 describe("<TimeLine>", () => {
-  it("renders today post default", () => {
-    const wrapper = mount(TimeLine)
+  it.only("mocked axios", async () => {
+    const result = await axios.get("/invalid-path")
+
+    expect(axios.patch).toBeUndefined()
+    expect(axios.get).toHaveBeenCalledWith("/invalid-path")
+    expect(axios.get).toBeCalledTimes(1)
+    expect(result).toMatchObject({
+      status: 200,
+      statusText: "OK",
+      data: { code: 200, data: null }
+    })
+  })
+
+  it("renders today post default", async () => {
+    const wrapper = mount({
+      components: { TimeLine },
+      template: `
+      <Suspense>
+        <template #default>
+          <TimeLine />
+        </template>
+        <template #fallback>
+          Loading...
+        </template>
+      </Suspense>
+      `
+    })
+    await flushPromises()
     console.log(wrapper.html())
     expect(wrapper.html()).toContain(today.created.format("Do MMM"))
   })
